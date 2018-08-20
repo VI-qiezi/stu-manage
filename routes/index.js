0eda0faf978d709182f10b33534f4a9f05ded9cc
VI-qiezi <1124629306@qq.com>
2018-08-20 09:45:08 +0800

    实现注销功能

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 var checkLogin = require("../modules/checkLogin.js");
 
 router.get('/', checkLogin, function(req, res, next) {
+  
   res.render('index', { title: '学生管理系统' });
 });
 
--- routes/users.js
+++ routes/users.js
@@ -69,4 +69,17 @@ router.post("/login", function(req, res, next){
   // console.log(type);
   // console.log(remember);
 })
+
+router.post("/logout", function(req, res, next){
+  // 清空session和cookie
+  req.session.user = null;
+  res.clearCookie("user");
+  // 向客户端相应数据，客户端在根据获取到的数据进行跳转，我们称之为“客户端跳转（渲染）”（推荐），客户端渲染可以把接口和视图分成两个独立的项目，有利于业务逻辑和视图分离。
+  res.json({code:200, message: "注销成功！"});
+  // 以下两种方式都是“服务器端跳转（渲染）”
+  // 方式一：直接跳转到login.ejs视图，缺点是代码和"/login"接口重复，并且浏览器上显示的是logout接口
+  // res.render("login", {title: "登录"});
+  // 方式二：解决了方式一的两个缺点，推荐使用。
+  // res.redirect("/login");
+})
 module.exports = router;
