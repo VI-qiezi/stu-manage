00e2b2ef67d9738432099807c3ea6cf1e7c14bbd
VI-qiezi <1124629306@qq.com>
2018-08-23 19:27:05 +0800

    完成教师添加和列表功能

--- public/javascripts/tea-add.js
+++ public/javascripts/tea-add.js
@@ -1,14 +1,34 @@
-$('#btnSave').click(function () {
+$('#btnSave').click(function (ev) {
+    ev.preventDefault();
     var tno = $.trim($("#tno").val());
     var name = $.trim($("#name").val());
     var sex = $.trim($("#sex").val());
     var birthday = $.trim($("#birthday").val());
     var card = $.trim($("#card").val());
-    var majorId = $.trim($("#majorId").val());
-    var classId = $.trim($("#classId").val());
-    var departId = $.trim($("#departId").val());
-    var data = { tno, name, sex, birthday, card, majorId, classId, departId };
-    $.post("/teachers/add", data, function (d) {
-        console.log(d);
+    // 把以下三个字段隐式转化为数字类型
+    var majorId = $.trim($("#majorId").val()) - 0;
+    var classId = $.trim($("#classId").val()) - 0;
+    var departId = $.trim($("#departId").val()) - 0;
+
+    if(!tno || !name || !sex || !birthday || !card || majorId == -1 || classId == -1 || departId  == -1){
+        $("#myModal .modal-body").text("教师编号，姓名，性别，生日，身份证号，所学专业，所属班级，所属院系不能为空！");
+        $("#myModal").modal();
+        return;
+    }
+
+    var nativePlace = $.trim($("#nativePlace").val());
+    var address = $.trim($("#address").val());
+    var qq = $.trim($("#qq").val());
+    var phone = $.trim($("#phone").val());
+    var email = $.trim($("#email").val());
+
+    var data = { tno, name, sex, birthday, card, majorId, classId, departId, nativePlace, address, qq, phone, email};
+    $.post("/teachers/add", data, function (data) {
+        if(data.code != 200){
+            $("#myModal .modal-body").text(data.message);
+            $("#myModal").modal();
+            return;
+        }
+        location.href = "/teachers/list";
     })
 })
--- routes/teachers.js
+++ routes/teachers.js
@@ -2,17 +2,118 @@ var express = require("express");
 
 var router = express.Router();
 
-router.get("/add", function(req, res, next){
-    res.render("teachers/add", {title: "教师添加"});
+var pool = require("../modules/db.js");
+var md5 = require("md5");
+var checkLogin = require("../modules/checkLogin.js");
+
+router.get("/add", checkLogin, function (req, res, next) {
+    var sql = `
+    SELECT * FROM majors WHERE status = 0;
+    SELECT * FROM classes WHERE status = 0;
+    SELECT * FROM departments WHERE status = 0;
+    `;
+    pool.query(sql, function(err, result){
+        if(err){
+            res.json({code:201, message: "数据库操作异常！"});
+            return;
+        }
+        res.render("teachers/add", { title: "教师添加", majors: result[0], classes: result[1], departs: result[2] });
+    })
 })
 
-router.post("/add", function(req, res, next){
-    
-    res.json({code:200, message: "添加成功！"});
+router.post("/add", checkLogin, function (req, res, next) {
+    var tno = req.body.tno;
+    var name = req.body.name;
+    var sex = req.body.sex;
+    var birthday = req.body.birthday;
+    var card = req.body.card;
+    var majorId = req.body.majorId;
+    var classId = req.body.classId;
+    var departId = req.body.departId;
+    var nativePlace = req.body.nativePlace;
+    var address = req.body.address;
+    var qq = req.body.qq;
+    var phone = req.body.phone;
+    var email = req.body.email;
+    // 服务器端判断
+    if (!tno || !name || !sex || !birthday || !card || majorId == -1 || classId == -1 || departId == -1) {
+        res.json({ code: 201, message: "教师编号，姓名，性别，生日，身份证号，所学专业，所属班级，所属院系不能为空！" });
+        return;
+    }
+
+    // 数据库操作
+    // 判断教师表中是否存在tno
+    pool.query(`SELECT * FROM teachers WHERE tno=?`, [tno], function (err, result) {
+        if (err) {
+            res.json({ code: 202, message: "数据库操作异常！" });
+            return;
+        }
+        if (result.length > 0) {
+            res.json({ code: 203, message: "你添加的教师已存在！" });
+            return;
+        }
+
+        // 向教师表和用户表中添加数据
+        var sql = `INSERT INTO teachers(tno, name, sex, birthday, card, majorId, classId, departId, nativePlace, address, qq, phone, email, status, createTime, createUserId) VALUE(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`;
+        var data = [tno, name, sex, birthday, card, majorId, classId, departId, nativePlace, address, qq, phone, email, 0, new Date(), req.session.user.id];
+        pool.query(sql, data, function (err, result1) {
+            if (err) {
+                res.json({ code: 204, message: "数据库操作异常！" });
+                return;
+            }
+            pool.query(`INSERT INTO users(loginName, password, type, status) VALUE(?,?,?,?)`, [tno, md5("123456"), 1, 0], function (err, result2) {
+                if (err) {
+                    res.json({ code: 205, message: "数据库操作异常！" });
+                    return;
+                }
+                res.json({ code: 200, message: "添加成功！" });
+            })
+        })
+
+    })
+
 })
 
-router.get("/list", function(req, res, next){
-    res.render("teachers/list", {title: "教师列表"});
+router.get("/list", checkLogin, function (req, res, next) {
+    var sql = `SELECT t.id,
+    t.tno, 
+    t.name, 
+    t.sex,
+    t.birthday,
+    t.card,
+    t.majorId,
+    t.classId,
+    t.departId,
+    t.nativePlace,
+    t.address,
+    t.qq,
+    t.phone,
+    t.email,
+    t.status,
+    t.createTime,
+    t.createUserId,
+    t.updateTime,
+    t.updateUserId,
+    d.name AS departName,
+    m.name AS majorName,
+    c.name AS className,
+    u1.loginName AS createUserName,
+    u2.loginName AS updateUserName 
+    FROM teachers t 
+    LEFT JOIN departments d ON t.departId = d.id 
+    LEFT JOIN majors m ON t.majorId = m.id 
+    LEFT JOIN classes c ON t.classId = c.id 
+    LEFT JOIN users u1 ON t.createUserId = u1.id
+    LEFT JOIN users u2 ON t.updateUserId = u2.id
+    WHERE (1=1) ORDER BY t.id;
+    `;
+    pool.query(sql, function(err, result){
+        if(err){
+            res.json({code:201, message: "数据库操作异常！"});
+            return;
+        }
+        res.render("teachers/list", { title: "教师列表", teachers: result });
+    })
 })
 
 module.exports = router;
